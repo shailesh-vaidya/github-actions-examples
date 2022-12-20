82fcc12c4ca7d716639e0c12c1c0cbd62289a56c
Shailesh Vaidya <shailesh.vaidya@seagate.com>
2022-12-20 19:15:17 +0530

    Remove unwanted lines
    Signed-off-by: Shailesh Vaidya <shailesh.vaidya@seagate.com>

--- test.sh
+++ test.sh
@@ -1,13 +1,3 @@
 ##/bin/bash
 
 echo "This is test"
-This is test#2
-This is test#3
-This is test#4
-This is test#5
-This is from UI
-This is from UI#2
-This is from UI#5
-This is from UI#5
-This is from UI#5
-This is from UI#6
